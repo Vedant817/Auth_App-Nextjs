b8b78b8d88c1b29727f4b8b20ce85b2a1b5f2873
Vedant Mahajan <vedantmahajan271@gmail.com>
2023-12-25 21:33:56 +0530

    Building backend for the signup page

---
+++ src/app/api/users/signup/route.ts
@@ -0,0 +1,31 @@
+import {connect} from "@/db_config/dbConfig";
+import User from "@/models/userModel"
+import { NextRequest, NextResponse } from "next/server";
+import bcryptjs from 'bcryptjs';
+
+connect()
+
+export async function POST(request: NextRequest) {
+    try {
+        const reqBody = await request.json();
+        const {username, email, password} = reqBody;
+        console.log(reqBody);
+        
+        //? Checking if user exists
+        const user = await User.findOne({email})
+        if(user){
+            return NextResponse.json({error: 'User Already exists'}, {status:400});
+        }
+        //? Hash Password
+        const salt = await bcryptjs.genSalt(10);
+        const hashedPassword = await bcryptjs.hash(password, salt);
+
+        const newUser = new User({username, email, password: hashedPassword});
+        const savedUser = await newUser.save()
+        console.log(savedUser);
+        return NextResponse.json({message: 'New User created Successfully', success: true, savedUser});
+
+    } catch (error: any) {
+        return NextResponse.json({error: error.message}, {status: 500})
+    }
+}
