33e7d8b20b351a1bd4254b4d6fec7f7433711479
Vedant Mahajan <vedantmahajan271@gmail.com>
2023-12-25 15:41:07 +0530

    Adding .env to gitignore

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,7 +1,23 @@
+"use client"
+import Link from "next/link"
+import React from "react"
+import { useRouter } from "next/navigation"
+import axios from "axios"
+
 export default function SignupPage (){
+    const [user, setUser] = React.useState({
+        email:"",
+        password:"",
+        username:"",
+    })
+
+    const onSignup = async () => {
+        
+    }
+
     return(
-        <div className="flex">
-            <h1 className="text-center text-white text-2xl">Sign Up</h1>
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+            <h1>Sign Up</h1>
         </div>
     )
 }
