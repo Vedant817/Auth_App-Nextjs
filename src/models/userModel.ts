365d11b110260db905fdb700bdcd3150bd191f80
Vedant Mahajan <vedantmahajan271@gmail.com>
2023-12-25 16:25:33 +0530

    Creating User Model for the Database

---
+++ src/models/userModel.ts
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+import { unique } from "next/dist/build/utils";
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, "Please provide a username"],
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: [true, "Please provide an email"],
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: [true, "Please provide a password"],
+    },
+    isVerified: {
+        type: Boolean,
+        default: false,
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
+    forgotPasswordToken: String,
+    forgotPasswordTokenExpiry: Date,
+    verifyToken: String,
+    verifyTokenExpiry: Date,
+})
+
+const User = mongoose.models.users || mongoose.model("users", userSchema); //? This is checking if a model is already created then use that else create a new model.
+
+export default User;
