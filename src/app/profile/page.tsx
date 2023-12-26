2b00d969e6f2d707f54d5d798026da1d36d00718
Vedant Mahajan <vedantmahajan271@gmail.com>
2023-12-26 10:37:58 +0530

    Managing the me route

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,4 +1,5 @@
 "use client"
+import React, {useState} from "react"
 import axios from "axios"
 import Link from "next/link"
 import toast from "react-hot-toast"
@@ -6,6 +7,7 @@ import { useRouter } from "next/navigation"
 
 export default function ProfilePage(){
     const router = useRouter();
+    const [data, setData] = React.useState("nothing")
     const logout = async ()=>{
         try {
             await axios.get('/api/users/logout');
@@ -16,11 +18,21 @@ export default function ProfilePage(){
             toast.error(error.message)
         }
     }
+
+    const getUserDetail = async ()=>{
+        const res = await axios.get('/api/users/me');
+        console.log(res.data);
+        setData(res.data.data._id);
+    }
+
     return(
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1>Profile</h1>
             <hr />
+            <h2 className="p-1 rounded bg-green-500">{data === "nothing" ? "Nothing":<Link href={`/profile/${data}`}>{data}</Link>}</h2>
+            <hr />
             <button className="bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={logout}>Logout</button>
+            <button className="bg-green-800 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={getUserDetail}>Get Details</button>
         </div>
     )
 }
